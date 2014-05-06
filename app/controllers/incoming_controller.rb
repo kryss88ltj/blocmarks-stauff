e60cfd27ca5d3d9428fe84405c474be25f513624
kryss88ltj <christine.stauffer@gmail.com>
2014-05-05 23:54:45 -0400

    adding mailgun

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,12 +5,27 @@ class IncomingController < ApplicationController
   def create
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
 
+    if request.method == 'POST':
+         sender    = request.POST.get('sender')
+         # recipient = request.POST.get('recipient')
+         subject   = request.POST.get('subject', '')
+
+         body_plain = request.POST.get('body-plain', '')
+
+    
+    return HttpResponse('OK')
+    @topic = Topic.where(title: "E-mail").first_or_create
+    @user = User.where(email: sender)
+    unless @user.nil?
+      @user.posts.create(url: body_plain, topic_id: @topic.id)
+    end  
     # You put the message-splitting and business
     # magic here. 
 
     # Assuming all went well. 
-    head 200
+    # head 200
   end
+
+
 end
